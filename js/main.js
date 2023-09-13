2ef3d2f37c289301a5b657d2e968cbec7f2ba467
Mariam Aoune <53101129+maaou99@users.noreply.github.com>
2023-09-13 15:08:51 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function Video(){
+  console.log('playinggg')
+  let vid = document.getElementById("myVideo");
+  vid.playbackRate = 2;
+
+}
+
+
+window.onload = function(){
+  console.log("loaded");
+  Video()
+
+  }
+
+
+
+ 
+
+
